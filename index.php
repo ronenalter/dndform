362ef5753dd0b104d72f5d40b2cc636219b72625
Ronen E. <ronen.alter@winner.com>
2013-08-23 07:10:29 +0300

    added clear table functionality

--- index.php
+++ index.php
@@ -1,21 +1,28 @@
 <?php
-    // connection
-    $link = mysqli_connect("localhost","ronen","123456","ronen") or die("Error " . mysqli_error($link));  
-
+    //ini_set('display_errors', 1);error_reporting(E_ALL);
+    $host = 'localhost';     
+    $user = 'root';         
+    $password = '123456';
+    $db = 'ronen';
     $armor = $attack = $posted = '';
-    if(isset($_POST['submit']) && !empty($_POST['submit'])){
-        $armor = $_POST['armor'];
+    // connection
+    $link = mysqli_connect($host, $user, $password, $db) or die("Error ".mysqli_error($link));  
+    if (isset($_POST['submit']) && !empty($_POST['submit'])) {
+        $armor  = $_POST['armor'];
         $attack = $_POST['attack'];
         // query
-        $query = "INSERT INTO `fields` (armor, attack) VALUES('$armor','$attack')" or die("Error in the consult.." . mysqli_error($link));;
-        $result = $link->query($query);
+        $query = "INSERT INTO `fields` (armor, attack) VALUES('$armor','$attack')" ;
+        $result = $link->query($query) or die("Error in the consult.." . mysqli_error($link));
+    } elseif (isset($_POST['clear']) && !empty($_POST['clear'])) {
+        $query = "TRUNCATE table fields";
+        $result = $link->query($query) or die("Error in the consult.." . mysqli_error($link));
     }
 
 ?><!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title></title>
+        <title>DND Form</title>
     </head>
     <body>
         <form action="" method="post"> 
@@ -23,19 +30,28 @@
              <input id="armor" name="armor" type="text" value="<?php echo $armor?>" />
             <label for="attack">Attack: </label>
              <input id="attack" name="attack" type="text" value="<?php echo $attack?>" />
-            <input type="submit" name="submit" value="submit" />
+            <input type="submit" name="submit" value="Add Record" />
+            <input type="submit" name="clear" value="Reset" />
         </form>  
-        <p>   
+        <div>   
         <?php 
         //display information:
         if(isset($result) && $result){
-            echo "Database modified. Displaying records:<br />";
-            while($row = mysqli_fetch_array($result)) {
-              echo "Armor:" . $row["armor"] . " Attack:".$row['attack']."<br />";
-            }  
+            echo "Database modified. <br />";
         }
+          $query = "SELECT * FROM `fields`" ;
+            $result = $link->query($query) or die("Error in the consult.." . mysqli_error($link));
+            echo "<strong>Displaying records:</strong><br />
+                <table border='1' cellpadding='1' cellspacing='1'>
+                    <thead>
+                        <tr><th>ID</th><th>Armor</th><th>Attack</th></tr>
+                    </thead><tbody>";
+            while($row = mysqli_fetch_array($result)) {
+              echo "<tr><td>{$row['id']}</td><td>{$row['armor']}</td><td>{$row['attack']}</td></tr>";
+            }  
+            echo "</tbody><table>";
         ?>
-        </p>
+        </div>
     </body>
 </html>
 <?php 
